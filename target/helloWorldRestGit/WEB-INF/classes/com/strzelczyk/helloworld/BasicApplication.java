35648e200bdedb2833c95bf990cfa77af11db51b
AdrianStr <adrian.strzelczyk@stud.fh-luebeck.de>
2017-05-20 20:08:04 +0200

    exception in heroku

--- target/helloWorldRestGit/WEB-INF/classes/com/strzelczyk/helloworld/BasicApplication.java
+++
@@ -1,9 +0,0 @@
-package com.strzelczyk.helloworld;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class BasicApplication extends Application{
-
-}
